24c7a48be21b264a793cc36186a3eec14d04b4a8
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-04-22 09:09:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from '../src/components/App';
+
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from "./redux/store";
 import { ModalProvider } from 'context/ModalContext/ModalContext';
-// import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { ThemeProvider } from 'styled-components';
-// import { theme } from 'styles';
+import { App } from 'components/App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
@@ -19,25 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ModalProvider>
   </Provider>
 
-  // <Provider
-  // // store={store}
-  // >
 
-  //   <ThemeProvider
-  //   //  theme={theme}
-  //   >
-  //     <PersistGate loading={null}
-  //     // persistor={persistor}
-  //     >
-  //       <BrowserRouter basename="/rental_of_campers">
-  //         <App />
-  //       </BrowserRouter>
-  //     </PersistGate>
-  //   </ThemeProvider >
-
-
-
-  // </Provider>
 
 );
 
